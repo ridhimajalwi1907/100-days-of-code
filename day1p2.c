083340a54d81ad5c11401b46674ba629b9299455
ridhimajalwi1907 <ridhijalwi1907@gmail.com>
2026-08-10 21:36:34 +0530

    Add files via upload

---
+++ day1p2.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a");
+    scanf("%d",&a);
+    printf("Enter b");
+    scanf("%d",&b);
+    printf("Sum = %d\n",a+b);
+    printf("Difference = %d\n",a-b);
+    printf("Product = %d\n",a*b);
+    printf("Quotient = %d\n",a/b);
+    return 0;
+}
+    
