dbce52d887804c60cd82f0092a274b4a497affc4
ridhimajalwi1907 <ridhijalwi1907@gmail.com>
2026-08-11 14:25:03 +0530

    Add files via upload

---
+++ day2p1.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main(){
+    int l,b;
+    printf("Enter l  :");
+    scanf("%d",&l);
+    printf("Enter b  :");
+    scanf("%d",&b);
+    printf("Area of rectangle is %d\n",l*b);
+    printf("Perimeter of rectangle is %d",2*(l+b));
+    return 0;
+}    
---
+++ day2p2.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main(){
+    float r;
+    printf("Enter radius :");
+    scanf("%f",&r);
+    printf("Area of circle is %f\n",3.14*r*r);
+    printf("Circumference of circle is %f",2*3.14*r);
+    return 0;
+}    
