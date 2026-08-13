a0af48f498c5ab9f582187d9a092687af66e8fcb
ridhimajalwi1907 <ridhijalwi1907@gmail.com>
2026-08-13 20:37:50 +0530

    Add files via upload

---
+++ day4p1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter first number :");
+    scanf("%d",&a);
+    printf("Enter second number :");
+    scanf("%d",&b);
+    printf("Before swapping : a = %d, b = %d\n",a,b);
+    printf("After swapping : a = %d, b = %d\n",b,a);
+    return 0;
+}
