b64eb335b911404cb40c8677daa777bf69407bb9
ridhimajalwi1907 <ridhijalwi1907@gmail.com>
2026-09-15 21:54:30 +0530

    Add files via upload

---
+++ day37p1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10], rowSum[10];
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate sum of each row
+    for (i = 0; i < rows; i++) {
+        rowSum[i] = 0;
+        for (j = 0; j < cols; j++) {
+            rowSum[i] += matrix[i][j];
+        }
+    }
+
+    printf("Sum of each row:\n");
+    for (i = 0; i < rows; i++) {
+        printf("Row %d = %d\n", i + 1, rowSum[i]);
+    }
+
+    return 0;
+}
