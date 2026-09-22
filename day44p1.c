eb6b948544a2722c9e021adb082de6450839d8e3
ridhimajalwi1907 <ridhijalwi1907@gmail.com>
2026-09-22 16:24:02 +0530

    Add files via upload

---
+++ day44p1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+
+        if (str[i] == ' ')
+            spaces++;
+
+        else if (str[i] >= '0' && str[i] <= '9')
+            digits++;
+
+        else if (!((str[i] >= 'A' && str[i] <= 'Z') ||
+                   (str[i] >= 'a' && str[i] <= 'z')))
+            special++;
+    }
+
+    printf("Spaces = %d\n", spaces);
+    printf("Digits = %d\n", digits);
+    printf("Special characters = %d\n", special);
+
+    return 0;
+}
